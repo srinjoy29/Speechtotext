8bde8dbc0b5caa5dab7e7166f65696a0d116925e
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2023-07-21 18:21:06 +0530

    Update Footer.jsx
    a line added

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
+      <p>This is a React.Js mini project , Its using 'react-speech-recognition' for converting speech into text. </p>
       <p>Copyright ⓒ {year}</p>
     </footer>
   );

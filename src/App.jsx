966890dd56355652d0d825ef72f763d120507e23
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2023-07-21 14:34:48 +0530

    Update App.jsx
    New button added as clear button

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,55 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useClipboard from "react-use-clipboard";
-import {useState} from "react";
+import { useState } from "react";
 
-
-function App(){
-  const [textToCopy, setTextToCopy] = useState();
+function App() {
+    const [textToCopy, setTextToCopy] = useState("");
     const [isCopied, setCopied] = useClipboard(textToCopy, {
-        successDuration:1000
+        successDuration: 1000
     });
+
     const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
-    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+    const { transcript, browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
 
     if (!browserSupportsSpeechRecognition) {
-        return null
+        return null;
     }
 
+    const handleCopyClick = () => {
+        setTextToCopy(transcript);
+        setCopied();
+    };
 
-  return (
-  <div>
-  <Header />
-  <div className="container">
-                <h2>   Speech to Text Converter   </h2>
-                <br/>
+    const handleClearClick = () => {
+        resetTranscript();
+        setTextToCopy("");
+    };
+
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <h2>Speech to Text Converter</h2>
+                <br />
                 <p>A React hook that converts speech from the microphone to text.</p>
 
-                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
+                <div className="main-content" onClick={() => setTextToCopy(transcript)}>
                     {transcript}
                 </div>
 
                 <div className="btn-style">
-
-                    <button onClick={setCopied}>
+                    <button onClick={handleCopyClick}>
                         {isCopied ? 'Copied!' : 'Copy to clipboard'}
                     </button>
                     <button onClick={startListening}>Start Listening</button>
                     <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
-
+                    <button onClick={handleClearClick}>Clear Text</button>
                 </div>
-
             </div>
-    <Footer />
-  </div>
-  );
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
-
-

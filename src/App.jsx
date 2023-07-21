977ba5f41e3d824bb927f1f80c608233d1dc1bf4
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2023-07-21 17:18:20 +0530

    Update App.jsx
    copy clipboard button removed as its not working properly

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import useClipboard from "react-use-clipboard";
 import { useState } from "react";
 
 function App() {
-    const [textToCopy, setTextToCopy] = useState("");
-    const [isCopied, setCopied] = useClipboard(textToCopy, {
-        successDuration: 1000
-    });
+ ;
 
     const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
     const { transcript, browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
@@ -18,14 +14,9 @@ function App() {
         return null;
     }
 
-    const handleCopyClick = () => {
-        setTextToCopy(transcript);
-        setCopied();
-    };
-
     const handleClearClick = () => {
         resetTranscript();
-        setTextToCopy("");
+      
     };
 
     return (
@@ -36,14 +27,9 @@ function App() {
                 <br />
                 <p>A React hook that converts speech from the microphone to text.</p>
 
-                <div className="main-content" onClick={() => setTextToCopy(transcript)}>
-                    {transcript}
-                </div>
+              
 
                 <div className="btn-style">
-                    <button onClick={handleCopyClick}>
-                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
-                    </button>
                     <button onClick={startListening}>Start Listening</button>
                     <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
                     <button onClick={handleClearClick}>Clear Text</button>

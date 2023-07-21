e931dcbd859fc462bdcadc41ab55252ca14af79c
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2023-07-21 17:34:16 +0530

    Update App.jsx
    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
+import { useState } from "react";
 
 function App() {
- ;
-
+    const [transcript, setTranscript] = useState("");
     const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
-    const { browserSupportsSpeechRecognition, resetTranscript } = useSpeechRecognition();
+    const { browserSupportsSpeechRecognition } = useSpeechRecognition();
 
     if (!browserSupportsSpeechRecognition) {
-        return null;
+        return null
     }
 
-    const handleClearClick = () => {
-        resetTranscript();
-      
+    const clearTranscript = () => {
+        setTranscript("");
     };
 
     return (
@@ -24,15 +22,17 @@ function App() {
             <Header />
             <div className="container">
                 <h2>Speech to Text Converter</h2>
-                <br />
+                <br/>
                 <p>A React hook that converts speech from the microphone to text.</p>
 
-              
+                <div className="main-content" onClick={() => setTranscript(transcript)}>
+                    {transcript}
+                </div>
 
                 <div className="btn-style">
                     <button onClick={startListening}>Start Listening</button>
                     <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
-                    <button onClick={handleClearClick}>Clear Text</button>
+                    <button onClick={clearTranscript}>Clear Text</button>
                 </div>
             </div>
             <Footer />
